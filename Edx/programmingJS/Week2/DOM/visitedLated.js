f8a3ae3197fc0aba2d298b47fdd5d444f2f163e3
Mcsb9 <Mcsb9@iMac-de-2A.local>
2017-09-28 15:44:36 -0500

    Aja

--- Edx/programmingJS/Week2/DOM/visitedLated.js
+++ Edx/programmingJS/Week2/DOM/visitedLated.js
@@ -1,7 +1,9 @@
-
+//Variables declaration
 var timesVisited =0;
 var lastVisit = 'never';
+var clicks = 0;
 
+//Show the lastVisit and the times that you have visited
 if(localStorage.lastVisit){
   var lastVisit = JSON.parse(localStorage.getItem('lastVisit'));
   timesVisited = lastVisit.numVisits;
@@ -17,3 +19,42 @@ myLastVisit.date = new Date();
 myLastVisit.numVisits = timesVisited;
 
 localStorage.lastVisit = JSON.stringify(myLastVisit);
+
+//Show the count of clicks numbers!
+function clickHandler(){
+  clicks++;
+  var numClicksSpan = document.getElementById('numClicks');
+  if(clicks == 1){
+    numClicksSpan.innerHTML = 'once';
+  }
+  else {
+    numClicksSpan.innerHTML = clicks + ' times';
+  }
+}
+var button = document.getElementById('clickMe');
+button.addEventListener('click', clickHandler);
+
+//Change the color of text when mouse put on a text
+
+function makeBold(){
+  var divs = document.getElementsByClassName('highlighText');
+  for(var i = 0; i < divs.length; i++){
+    divs[i].style.color = 'red';
+    divs[i].style.fontWeight = 'bold';
+  }
+}
+
+function restore(){
+  var divs = document.getElementsByClassName('highlighText');
+  for (var i = 0; i < divs.length; i++) {
+    divs[i].style.color = 'black';
+    divs[i].style.fontWeight = 'normal';
+
+  }
+}
+
+var mouseOverMeDiv = document.getElementById('mouseOverMe');
+mouseOverMeDiv.addEventListener('mouseover',makeBold);
+mouseOverMeDiv.addEventListener('mouseout',restore);
+
+//Change the name of the text
