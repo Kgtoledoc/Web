28eefc2f9174239417c14370fec1b3cea68869ee
Mcsb9 <Mcsb9@iMac-de-2A.local>
2017-09-28 11:46:42 -0500

    Nuevos cambios

---
+++ Edx/programmingJS/Week2/DOM/visitedLated.js
@@ -0,0 +1,19 @@
+
+var timesVisited =0;
+var lastVisit = 'never';
+
+if(localStorage.lastVisit){
+  var lastVisit = JSON.parse(localStorage.getItem('lastVisit'));
+  timesVisited = lastVisit.numVisits;
+  lastVisitDate = lastVisit.date;
+}
+document.getElementById('lastVisitDate').innerHTML = lastVisitDate;
+
+timesVisited++;
+document.getElementById('report').innerHTML = timesVisited;
+
+var myLastVisit = {};
+myLastVisit.date = new Date();
+myLastVisit.numVisits = timesVisited;
+
+localStorage.lastVisit = JSON.stringify(myLastVisit);
